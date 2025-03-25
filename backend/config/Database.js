7d114d5bf3ff598b53538e4c2b4f358fb2cc9af6
ArvidionHavas <142262748+Arvidion@users.noreply.github.com>
2025-03-25 16:48:51 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('nebeng','root','',{
+const db = new Sequelize('RECOVER_YOUR_DATA','root','',{
     host: "34.133.56.122",
     dialect: 'mysql'
 });
